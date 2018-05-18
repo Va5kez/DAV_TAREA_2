2e66aec9e0c5ea006c1307c84f02df8f2c96888c
Victor Vasquez <victor.vasquez96@unitec.edu>
2018-05-18 12:25:04 -0600

    Separated ProductList and Product into diferent files

--- public/js/ProductList.jsx
+++ public/js/ProductList.jsx
@@ -1,7 +1,14 @@
+/*import React, {Component} from 'react'
+import Product from './Product'*/
+
 class ProductList extends React.Component {
-  state = {
-    products: [],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
 
   componentDidMount() {
     this.setState({ products: Seed.products });
@@ -46,47 +53,3 @@ class ProductList extends React.Component {
     );
   }
 }
-
-class Product extends React.Component {
-  handleUpVote = () => (
-    this.props.onVote(this.props.id)
-  );
-
-  render() {
-    return (
-      <div className='item'>
-        <div className='image'>
-          <img src={this.props.productImageUrl} />
-        </div>
-        <div className='middle aligned content'>
-          <div className='header'>
-            <a onClick={this.handleUpVote}>
-              <i className='large caret up icon' />
-            </a>
-            {this.props.votes}
-          </div>
-          <div className='description'>
-            <a href={this.props.url}>
-              {this.props.title}
-            </a>
-            <p>
-              {this.props.description}
-            </p>
-          </div>
-          <div className='extra'>
-            <span>Submitted by:</span>
-            <img
-              className='ui avatar image'
-              src={this.props.submitterAvatarUrl}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <ProductList />,
-  document.getElementById('content')
-);
---
+++ public/js/app.jsx
@@ -0,0 +1,6 @@
+/*import {render} from 'react-dom'
+render(<ProductList />, document.getElementById('content'));*/
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
